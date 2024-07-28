8e372eed9b3f8707c035fb798c49544cb8fd7c05
ADDY69O <kohsa9211420@gmail.com>
2024-07-28 12:11:01 +0530

    added infinite scroll & pagination

---
+++ logic.js
@@ -0,0 +1,150 @@
+let currentPage = 1;
+let totalLimit = 10;
+let totalRecords = 0;
+let totalPages = 0;
+
+const getProducts = async (page, limit, prev = false) => {
+  try {
+    let response;
+    if (prev) {
+      response = await fetch(
+        `https://dummyjson.com/posts?limit=${totalLimit * page}&skip=${
+          (1 - 1) * limit
+        }`
+      );
+    } else {
+      response = await fetch(
+        `https://dummyjson.com/posts?limit=${totalLimit}&skip=${
+          (page - 1) * limit
+        }`
+      );
+    }
+    let data = await response.json();
+
+    if (totalRecords === 0) {
+      totalRecords = data.total;
+      totalPages = Math.ceil(totalRecords / totalLimit);
+
+      let paginationDiv = document.getElementsByClassName("pageBtn")[0];
+
+      for (let i = 1; i <= totalPages; i++) {
+        let newPage = document.createElement("div");
+        newPage.innerText = i;
+        newPage.setAttribute("class", "pages");
+        newPage.addEventListener("click", (e) => {
+          currentPage = Number(e.target.innerText);
+          updatePageStyles();
+          updateButtonStates();
+          getProducts(currentPage, totalLimit, true);
+        });
+        paginationDiv.appendChild(newPage);
+      }
+    }
+
+    let productDiv = document.getElementsByClassName("Products")[0];
+    if (prev) {
+      productDiv.innerHTML = "";
+      window.scrollTo(0, 0);
+    }
+    data.posts.forEach((product) => {
+      let newDiv = document.createElement("div");
+      newDiv.setAttribute("class", "Product");
+
+      let title = document.createElement("h1");
+      title.setAttribute("class", "productTitle");
+      title.innerText = product.title;
+
+      let body = document.createElement("p");
+      body.setAttribute("class", "productBody");
+      body.innerText = product.body;
+
+      let tagDiv = document.createElement("div");
+      tagDiv.setAttribute("class", "productTag");
+
+      let tagTitle = document.createElement("h3");
+      tagTitle.setAttribute("class", "tagTitle");
+      tagTitle.innerText = "Tags:";
+
+      tagDiv.appendChild(tagTitle);
+
+      product.tags.forEach((tag) => {
+        let tagEle = document.createElement("p");
+        tagEle.setAttribute("class", "tagEle");
+        tagEle.innerText = tag;
+        tagDiv.appendChild(tagEle);
+      });
+
+      let views = document.createElement("p");
+      views.setAttribute("class", "productViews");
+      views.innerText = `Views: ${product.views}`;
+
+      let reactions = document.createElement("p");
+      reactions.setAttribute("class", "productReactions");
+      reactions.innerHTML = `Likes: ${product.reactions.likes} | Dislikes: ${product.reactions.dislikes}`;
+
+      newDiv.appendChild(title);
+      newDiv.appendChild(document.createElement("hr"));
+      newDiv.appendChild(body);
+      newDiv.appendChild(tagDiv);
+      newDiv.appendChild(views);
+      newDiv.appendChild(reactions);
+
+      productDiv.appendChild(newDiv);
+    });
+
+    updatePageStyles();
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return;
+  }
+};
+
+const loadInitialProducts = () => {
+  getProducts(currentPage, totalLimit);
+};
+
+document.addEventListener("DOMContentLoaded", loadInitialProducts);
+
+document.querySelector(".Products").addEventListener("scroll", (event) => {
+  let { clientHeight, scrollHeight, scrollTop } = event.target;
+  if (
+    clientHeight + scrollTop + 1 >= scrollHeight &&
+    currentPage < totalPages
+  ) {
+    currentPage += 1;
+    updateButtonStates();
+    getProducts(currentPage, totalLimit);
+  }
+});
+
+const handleNext = () => {
+  if (currentPage < totalPages) {
+    currentPage += 1;
+    updateButtonStates();
+    getProducts(currentPage, totalLimit);
+  }
+};
+
+const handleBack = () => {
+  if (currentPage > 1) {
+    currentPage -= 1;
+    updateButtonStates();
+    getProducts(currentPage, totalLimit, true);
+  }
+};
+
+const updateButtonStates = () => {
+  document.querySelector(".btnPrev").disabled = currentPage === 1;
+  document.querySelector(".btnNext").disabled = currentPage === totalPages;
+  updatePageStyles();
+};
+
+const updatePageStyles = () => {
+  document.querySelectorAll(".pages").forEach((page, index) => {
+    if (index + 1 === currentPage) {
+      page.classList.add("active");
+    } else {
+      page.classList.remove("active");
+    }
+  });
+};
