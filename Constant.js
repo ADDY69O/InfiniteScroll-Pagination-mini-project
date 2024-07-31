1ac426c4559ef1951a6d65bba625168506688668
ADDY69O <kohsa9211420@gmail.com>
2024-07-31 10:36:55 +0530

    Reverting back merge changes Revert "Merge pull request #2 from ADDY69O/Resolved_bugs"
    This reverts commit 9a6f59fac6b46d22ad825831a6cf2317d6c3f0bc, reversing
    changes made to dbf02993c1543c6f67d4d7f5cb4494588f3f99fa.

--- Constant.js
+++
@@ -1,6 +0,0 @@
-const getLocalUserData = () => localStorage.getItem("userInfo");
-const setLocalUserData = (data) => localStorage.setItem("userInfo", data);
-
-const LocalStoargeKeys = { USERINFO: "userInfo" };
-
-export { getLocalUserData, setLocalUserData, LocalStoargeKeys };
--- Dom.js
+++
@@ -1,9 +0,0 @@
-const domCreateElement = (tag, classname, text = false) => {
-  let newEle = document.createElement(tag);
-  newEle.setAttribute("class", classname);
-
-  if (text) {
-    newEle.innerText = text;
-  }
-  return newEle;
-};
